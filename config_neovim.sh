b82b24766390ab2042b81bdf003d2382e11bc1d7
Ying Zou <xpzouying@gmail.com>
2017-03-04 22:38:39 +0800

    Update shell script.

--- config_neovim.sh
+++ config_neovim.sh
@@ -9,13 +9,13 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 NEOVIM_PATH="$HOME/.config/nvim"
 NEOVIM_PLUGGED_PATH=$NEOVIM_PATH"/plugged"
 
-if [ ! -d $NEOVIM_PLUGGED_PATH ]; then
+if [ ! -d "$NEOVIM_PLUGGED_PATH" ]; then
     echo "Create dir for neovim..."
-    mkdir -p $NEOVIM_PLUGGED_PATH
-    echo $NEOVIM_PLUGGED_PATH
+    mkdir -p "$NEOVIM_PLUGGED_PATH"
+    echo "$NEOVIM_PLUGGED_PATH"
 fi
 
 # Link neovim config
-ln -s $(pwd)/init.vim  $NEOVIM_PATH"/init.vim"
+ln -s "$(pwd)/init.vim"  "$NEOVIM_PATH/init.vim"
 
 
